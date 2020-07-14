ec280c650503061e80c72ee240b6f7a3b42aad46
Thomas Woertman <twoertman@gmail.com>
2020-07-14 22:57:50 +0200

    Improve Vulkan Loader error checking

--- vulkan/loader/loader.cpp
+++ vulkan/loader/loader.cpp
@@ -1,11 +1,12 @@
-#include <dlfcn.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <string>
+
 #include "../../vulkan-headers/include/vulkan/vulkan.h"
 #include "../../common/print.hpp"
 
 namespace {
-    
     template<typename... Args>
     [[noreturn]]
     void panic(const char* reason, Args&&... args){
@@ -17,27 +18,32 @@ namespace {
 
     class Loader {
         public:
-        Loader(){
-            const char* driver = "vulkan/soft_render_icd/libvulkan_granite.so";//"/usr/lib/libvulkan_granite.so";
-            driver_handle = dlopen(driver, RTLD_LAZY);
+        Loader(const std::string& driver_path){
+            driver_handle = dlopen(driver_path.c_str(), RTLD_LAZY);
 
             if(!driver_handle)
-                panic("Couldn't open driver: {:s} error: {:d}", driver, dlerror());
+                panic("Couldn't open driver path: {}, error: {}", driver_path, dlerror());
         }
 
         ~Loader(){
-            dlclose(driver_handle);
+            auto ret = dlclose(driver_handle);
+            if(ret != 0)
+                panic("Couldn't close driver, error: {}", dlerror());
         }
 
-        PFN_vkVoidFunction get_proc_addr(const char* name){
-            return (PFN_vkVoidFunction)dlsym(driver_handle, name);
+        PFN_vkVoidFunction get_proc_addr(const std::string& str){
+            auto f = (PFN_vkVoidFunction)dlsym(driver_handle, str.c_str());
+            if(const auto* err = dlerror(); err)
+                panic("Couldn't load driver function: {}", err);
+
+            return f;
         }
 
         private:
         void* driver_handle;
     };
 
-    Loader global_loader;
+    Loader global_loader{"vulkan/soft_render_icd/libvulkan_granite.so"};
 }
 
 extern "C" {
@@ -45,6 +51,7 @@ extern "C" {
         static auto* f = (PFN_vkEnumerateInstanceVersion)global_loader.get_proc_addr("vkEnumerateInstanceVersion");
         if(f)
             return f(pApiVersion);
+        
         panic("Couldn't find {:s} in loaded driver", __func__);
     }
 
@@ -52,6 +59,7 @@ extern "C" {
         static auto* f = (PFN_vkEnumerateInstanceLayerProperties)global_loader.get_proc_addr("vkEnumerateInstanceLayerProperties");
         if(f)
             return f(pPropertyCount, pProperties);
+        
         panic("Couldn't find {:s} in loaded driver", __func__);
     }
 
@@ -59,6 +67,7 @@ extern "C" {
         static auto* f = (PFN_vkEnumerateInstanceExtensionProperties)global_loader.get_proc_addr("vkEnumerateInstanceExtensionProperties");
         if(f)
             return f(pLayerName, pPropertyCount, pProperties);
+        
         panic("Couldn't find {:s} in loaded driver", __func__);
     }
 
@@ -66,6 +75,7 @@ extern "C" {
         static auto* f = (PFN_vkCreateInstance)global_loader.get_proc_addr("vkCreateInstance");
         if(f)
             return f(pCreateInfo, pAllocator, pInstance);
+        
         panic("Couldn't find {:s} in loaded driver", __func__);
     }
 }
